2aca8afa5554eda78b3430d1c60f51c85d33dd50
siddiqabu <35294835+siddiqabu@users.noreply.github.com>
2018-03-12 15:43:08 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    char c;
+    printf("Enter a character: ");
+
+   
+    scanf("%c", &c);  
+    
+   
+    printf("ASCII value of %c = %d", c, c);
+    return 0;
+}
+© 2018 GitHub, Inc.
